22b064e3a5a0b21b30dabf94ebe4179c849bcc30
Nicholas Sim <nicholassimws@gmail.com>
2019-03-18 20:46:59 +0000

    lib: add notice

--- lib.rs
+++ lib.rs
@@ -1,3 +1,16 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+//
+// This library, libcore_cheri, is intended to contain a minimal set
+// of Rust libcore code to work with CHERI temporarily.
+
 #![feature(no_core)]
 #![feature(lang_items)]
 #![no_core]
